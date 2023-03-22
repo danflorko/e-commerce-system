024afbf1bd3d3a6395a3d235f5d8e024ebd9004d
Daniil Florko <florko.danylo@gmail.com>
2023-03-22 21:58:06 +0200

    feat/redux-persist-local-storage

--- client/app/shared/store.ts
+++ client/app/shared/store.ts
@@ -1,16 +1,30 @@
 import { Store } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { createWrapper } from 'next-redux-wrapper';
 import rootReducer from "../reducers";
+import {
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { useSelector, TypedUseSelectorHook } from "react-redux";
 
-const makeStore: Store = configureStore({
-  reducer: rootReducer,
+const rootPersistConfig = {
+  key: 'rootStorage',
+  storage,
+  whitelist: ["cart"],
+};
+
+const pReducer = persistReducer(rootPersistConfig, rootReducer);
+
+const store: Store = configureStore({
+  reducer: pReducer,
   devTools: true,
 });
 
-export const wrapper = createWrapper(makeStore);
+const persistor = persistStore(store);
+
+export { persistor, store };
 
 export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof makeStore.dispatch;
+export type AppDispatch = typeof store.dispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
